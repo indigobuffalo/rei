2adb0fbe5cdf7b61e6b8a4b30b94bd51deb6a167
indigobuffalo <stephen.akerson@gmail.com>
2020-01-26 21:40:49 -0800

    improve rei data scraping

--- get_rei.py
+++ get_rei.py
@@ -1,9 +1,18 @@
+"""
+Working for saratoga but not others
+"""
+import sys
+from unicodedata import normalize
+
 import requests
 from bs4 import BeautifulSoup
-
+from pprint import pprint
 
 BASE_URL = "https://www.rei.com/stores/{store}.html"
 BASE_URL_2 = "https://www.rei.com/events/86150/members-only-garage-sale/"
+
+GS_BLOB_START = '"name" : "members only garage sale!",'
+GS_BLOB_END = u'"addresscountry" : "us"'
 
 STORE_MAP = {
     "berkeley": "269055",
@@ -12,25 +21,34 @@ STORE_MAP = {
     "sf":       "266084",
 }
 
-resp = requests.get(BASE_URL.format(store='saratoga'))
-soup = BeautifulSoup(resp.content, 'html.parser')
-text = soup.get_text()
-text_lines = text.split('\n')
 
-times = []
-garage_sales = []
-for idx, line in enumerate(text.split('\n')):
-    if '9:30' in line:
-        times.append((idx,line))
-    if 'garage sale' in line.strip().lower():
-        garage_sales.append(line)
+def contains_key_detail(line):
+    return any(x in line for x in ['startdate', 'enddate', 'telephone'])
 
+def get_garage_sale_dates(store):
+    resp = requests.get(BASE_URL_2 + STORE_MAP[store])
+    resp.raise_for_status()
 
-with open('yooo.txt', 'w') as tf:
-    tf.write(str(resp.content))
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    text = soup.get_text()
+    text_lines = [line.strip().lower() for line in text.split('\n')]
 
+    garage_sale = []
+    found_garage_sale = False
+    for line in text_lines:
+        if line == GS_BLOB_START:
+            found_garage_sale = True
+        if found_garage_sale: 
+            if contains_key_detail(line):
+                garage_sale.append(line)
+            if line == GS_BLOB_END:
+                break
 
-import ipdb
-ipdb.set_trace()
+    return garage_sale
 
+for store in sys.argv[1:]:
+  print(f"Getting garage sale info for {store}...\n")
+  details = get_garage_sale_dates(store)
+  pprint(details)
+  print("\n")
 
