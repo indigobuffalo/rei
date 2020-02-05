6b6f055d326e07465fdecc2f754557c21b76489b
indigobuffalo <stephen.akerson@gmail.com>
2020-02-04 21:12:22 -0800

    prettify the output

--- get_rei.py
+++ get_rei.py
@@ -1,12 +1,10 @@
-"""
-Working for saratoga but not others
-"""
 import sys
-from unicodedata import normalize
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+from typing import Dict, List, Tuple, Union
 
 BASE_URL = "https://www.rei.com/stores/{store}.html"
 BASE_URL_2 = "https://www.rei.com/events/86150/members-only-garage-sale/"
@@ -23,9 +21,47 @@ STORE_MAP = {
 
 
 def contains_key_detail(line):
-    return any(x in line for x in ['startdate', 'enddate', 'telephone'])
+    key_details = [
+        'name',
+        'startdate',
+        'enddate',
+        'telephone',
+        'url',
+        'streetaddress',
+        'addresslocality',
+    ]
+    return any(x in line for x in key_details)
 
-def get_garage_sale_dates(store):
+def prettify_dict(garage_sale: Dict) -> Dict:
+    street, city = garage_sale.pop('Streetaddress'), garage_sale.pop('Addresslocality')
+    garage_sale['address'] = f"{street.title()}, {city.title()}"
+
+    start_date, start_time_24H = garage_sale.pop('Startdate').split('T')
+    end_date, end_time_24H = garage_sale.pop('Enddate').split('T')
+    start_time = datetime.strptime(start_time_24H.split('-')[0], '%H:%M').strftime('%I:%M %p')
+    end_time = datetime.strptime(end_time_24H.split('-')[0], '%H:%M').strftime('%I:%M %p')
+    garage_sale['date'] = datetime.strptime(start_date, '%Y-%m-%d').strftime('%B %d, %Y')
+    garage_sale['time'] = f'{start_time} - {end_time}'
+
+    phone = garage_sale.pop('Telephone')
+    garage_sale['phone'] = f'({phone[2:5]}) {phone[5:8]}-{phone[8:13]}'
+
+    url = garage_sale.pop('Url')
+    garage_sale['url'] = url.lower()
+
+    name = garage_sale.pop('Name')
+    garage_sale['name'] = name
+
+    return garage_sale
+
+def scrub_key_val(key_and_val: List[str]) -> Tuple[str, str]:
+    k, v = key_and_val
+    k_scrubbed = k.replace('"', '').strip().title()
+    v_scrubbed = v.replace('"', '').replace(',', '').strip().title()
+
+    return k_scrubbed, v_scrubbed
+
+def get_garage_sale_dates(store, debug:bool = False) -> Union[Dict, List]:
     resp = requests.get(BASE_URL_2 + STORE_MAP[store])
     resp.raise_for_status()
 
@@ -33,22 +69,35 @@ def get_garage_sale_dates(store):
     text = soup.get_text()
     text_lines = [line.strip().lower() for line in text.split('\n')]
 
-    garage_sale = []
+    gs_lines = []
+    garage_sale = {}
     found_garage_sale = False
     for line in text_lines:
-        if line == GS_BLOB_START:
-            found_garage_sale = True
-        if found_garage_sale: 
-            if contains_key_detail(line):
-                garage_sale.append(line)
+        if found_garage_sale:
+            if debug:
+                gs_lines.append(line)
+            else:
+                if contains_key_detail(line):
+                    k, v = scrub_key_val(line.split(':', 1))
+                    garage_sale[k] = v
             if line == GS_BLOB_END:
                 break
+        if line == GS_BLOB_START:
+            found_garage_sale = True
+    return gs_lines if debug else garage_sale
 
-    return garage_sale
+def main(stores: List, debug: bool = False) -> None:
+    for store in stores:
+      details = get_garage_sale_dates(store, debug)
+      if debug:
+          pprint(details)
+      else:
+          pprint(prettify_dict(details))
+          print('\n')
 
-for store in sys.argv[1:]:
-  print(f"Getting garage sale info for {store}...\n")
-  details = get_garage_sale_dates(store)
-  pprint(details)
-  print("\n")
 
+
+if __name__ == "__main__":
+    stores = sys.argv[1:] if len(sys.argv) > 1 else list(STORE_MAP.keys())
+    main(stores)
+#     main(stores, True)
