8759befa6a11f372aea3817dc454c19b76164a77
indigobuffalo <stephen.akerson@gmail.com>
2020-03-10 21:13:58 -0700

    squash them bugs

--- dashboard/nhl.py
+++ dashboard/nhl.py
@@ -12,25 +12,26 @@ Examples:
   get_nhl.py 7 
     
 Options:
-  --start=<start_date>    Start date, in format %Y-%m-%d.
-  --end=<end_date>        End date, in format %Y-%m-%d.
-  --year=<year>           Year of start and end dates, in format %Y.
+  --start=<start_date>     Start date, in format %Y-%m-%d.
+  --end=<end_date>         End date, in format %Y-%m-%d.
+  --year=<year>            Year of start and end dates, in format %Y.
 
 """
-
+import os
 from collections import defaultdict
 from datetime import date, datetime, timedelta, timezone
-from pprint import pprint
+import pprint
 from typing import Dict, Tuple
+import pathlib
+
 import requests
-import os
 
 import pytz
 from pytz import timezone
 from docopt import docopt
 
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+CURRENT_DIR = pathlib.Path(__file__).parent.absolute()
 STATS_URL = 'https://statsapi.web.nhl.com'
 
 
@@ -103,18 +104,22 @@ def parse_player_stats(info, skaters, goalies):
     first, last = name.split(" ", 1)
     name_last_first = f'{last}, {first}'
     if 'skaterStats' in info['stats']:
+        skaters[name_last_first]['games'] += 1
         stats = parse_skater_stats(info['stats']['skaterStats'])
         for stat in stats:
             skaters[name_last_first][stat[0]] += stat[1]
     if 'goalieStats' in info['stats']:
+        goalies[name_last_first]['games'] += 1
         stats = parse_goalie_stats(info['stats']['goalieStats'])
         for stat in stats:
             goalies[name_last_first][stat[0]] += stat[1]
     return skaters, goalies
 
 
-def parse_stats(feed: Dict, skaters: Dict, goalies: Dict) -> Tuple[Dict, Dict]:
-    resp = requests.get(feed).json()
+def parse_stats(feed: Dict, session: requests.session) -> Tuple[Dict, Dict]:
+    skaters = defaultdict(lambda: defaultdict(int))
+    goalies = defaultdict(lambda: defaultdict(int))
+    resp = session.get(feed).json()
     box = resp['liveData']['boxscore']['teams']
     home_players = box['home']['players']
     away_players = box['away']['players']
@@ -122,40 +127,49 @@ def parse_stats(feed: Dict, skaters: Dict, goalies: Dict) -> Tuple[Dict, Dict]:
         skaters, goalies = parse_player_stats(info, skaters, goalies)
     for player, info in away_players.items():
         skaters, goalies = parse_player_stats(info, skaters, goalies)
-    return skaters, goalies
+    return dd_to_regular(skaters), dd_to_regular(goalies)
 
 
 def main(args):
     start, end = get_start_and_end_dates(args)
     stats_url = f"{STATS_URL}/api/v1/schedule?startDate={start}&endDate={end}"
-    resp = requests.get(stats_url).json()
+
+    # TODO: find a nice way to make session global
+    session = requests.session()
+
+    resp = session.get(stats_url).json()
 
     game_feeds = defaultdict(set)
     for date in resp['dates']:
-        print(f'See {date["totalGames"]} games on {date["date"]}')
+        print(f'Collecting stats for {date["totalGames"]} games on {date["date"]}')
         for game in date['games']:
             date, feed = parse_date_and_feed(game)
             game_feeds[date].add(feed)
     game_feeds = dict(game_feeds)
 
-    skaters = defaultdict(lambda: defaultdict(int))
-    goalies = defaultdict(lambda: defaultdict(int))
-    for date, feeds in game_feeds.items():
-        skaters, goalies = parse_stats(feeds.pop(), skaters, goalies)
+    skaters_total = {}
+    goalies_total = {}
+    for _, feeds in game_feeds.items():
+        for feed in feeds:
+            skaters, goalies = parse_stats(feed, session)
+            for skater, stats in skaters.items():
+                if skater in skaters_total:
+                    for stat in stats:
+                        skaters_total[skater][stat] += stats[stat]
+                else:
+                    skaters_total[skater] = stats
+            for goalie, stats in goalies.items():
+                if goalie in goalies_total:
+                    for stat in stats:
+                        goalies_total[goalie][stat] += stats[stat]
+                else:
+                    goalies_total[goalie] = stats
 
-    print(f'Collected stats from {start} to {end}')
-    print(f'Game feeds: {len(game_feeds)}')
-    print(f'Skaters: {len(skaters)}')
-    print(f'Goalies: {len(goalies)}')
-
-    print('\n--------------')
-    print('Skater stats:')
-    print('--------------\n')
-    pprint(dd_to_regular(skaters))
-    print('\n--------------')
-    print('Goalie stats:')
-    print('--------------\n')
-    pprint(dd_to_regular(goalies))
+    out_filename = f'players_{start}_to_{end}.txt'
+    out_file = os.path.join(CURRENT_DIR, out_filename)
+    with open(os.path.join(CURRENT_DIR, out_file), 'w') as f:
+        f.write(pprint.pformat(skaters_total))
+        f.write(pprint.pformat(goalies_total))
 
 
 if __name__ == '__main__':
